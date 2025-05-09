fc75a929ef024449c22180463e20ba87714c7c93
Kevin F <Freymaurer@gmx.de>
2025-05-09 14:51:17 +0200

    rmv temp files

---
+++ src/Constants.fs
@@ -0,0 +1,6 @@
+module App.Constants
+
+module URL =
+
+    [<Literal>]
+    let GitHub = @"https://github.com/Freymaurer/fa-poc"
--- src/fableoutput/Main.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { View_Main } from "./View.js";
-import "../tailwind.css";
-
-
-export const root = createRoot(document.getElementById("feliz-app"));
-
-root.render(View_Main());
-
